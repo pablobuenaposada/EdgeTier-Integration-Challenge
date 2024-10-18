2458a1f4562c6dfb3786dd591262e7c92d1d74a0
pablobuenaposada <pablo.buenaposada@gmail.com>
2024-10-18 17:14:47 +0200

    refactor start event

--- integration/events/events.py
+++ integration/events/events.py
@@ -6,9 +6,13 @@ from integration.constants import OUR_API
 from integration.events.constants import EVENT_START
 
 
+def create_chat(external_id, started_at, logger):
+    data = {"external_id": external_id, "started_at": started_at}
+    chat = requests.post(f"{OUR_API}/chats", json=data)
+    logger.info(f"Created chat {chat.json()['chat_id']}.")
+
+
 def process_events(events, logger):
     for event in events:
         if event["event_name"] == EVENT_START:
-            data = {"external_id": str(event["conversation_id"]), "started_at": datetime.now().isoformat()}
-            chat = requests.post(f"{OUR_API}/chats", json=data)
-            logger.info(f"Created chat {chat.json()['chat_id']}.")
+            create_chat(str(event["conversation_id"]), datetime.now().isoformat(), logger)
