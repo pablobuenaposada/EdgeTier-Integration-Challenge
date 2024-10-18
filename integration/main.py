9ce2c7548e2b9a2f1df304134a44f043ef014524
pablobuenaposada <pablo.buenaposada@gmail.com>
2024-10-18 13:06:32 +0200

    taking care of bigchat pagination

--- integration/main.py
+++ integration/main.py
@@ -13,18 +13,28 @@ logger.setLevel(logging.INFO)
 BIG_CHAT_API = "http://localhost:8267"
 OUR_API = "http://localhost:8266"
 
-def main():
-    # Get events from BigChat.
-    parameters = {"start_at": (datetime.now() - timedelta(seconds=10))}
-    events = requests.get(f"{BIG_CHAT_API}/events", params=parameters)
 
-    # TODO: This is just an example, feel free to change however you want:
-    for event in events.json()["events"]:
+def process_events(events):
+    for event in events:
         if event["event_name"] == "START":
-            # Create a chat.
             data = {"external_id": str(event["conversation_id"]), "started_at": datetime.now().isoformat()}
             chat = requests.post(f"{OUR_API}/chats", json=data)
             logger.info(f"Created chat {chat.json()['chat_id']}.")
+
+
+def main():
+    parameters = {"start_at": (datetime.now() - timedelta(seconds=10))}
+    response = requests.get(f"{BIG_CHAT_API}/events", params=parameters)
+    response_data = response.json()
+    process_events(response_data["events"])
+
+    # if more pages found we process also those
+    next_page_url = response_data.get("nextPageUrl")
+    while next_page_url:
+        response = requests.get(next_page_url)
+        response_data = response.json()
+        process_events(response_data["events"])
+        next_page_url = response_data.get("nextPageUrl")
 
 
 if __name__ == "__main__":
--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -1,28 +1,54 @@
 from integration import main
-from unittest.mock import patch, MagicMock, ANY
+from unittest.mock import patch, MagicMock, ANY, call
+
+from integration.main import BIG_CHAT_API, OUR_API
 
 
 class TestMain:
-    @patch('requests.get')
-    @patch('requests.post')
+
+    @patch("requests.get")
+    @patch("requests.post")
     def test_start(self, m_post, m_get):
         conversation_id = 12345
         m_get.return_value = MagicMock(
-            json=lambda: {"events": [{"event_name": "START", "conversation_id": conversation_id}]},
-            status_code=200
-        )
-        m_post.return_value = MagicMock(
-            json=lambda: {"chat_id": "1"},
-            status_code=201
+            json=lambda: {"events": [{"event_name": "START", "conversation_id": conversation_id}]}, status_code=200
         )
 
         main.main()
 
-        m_get.assert_called_once_with(
-            "http://localhost:8267/events",
-            params={'start_at': ANY}
-        )
+        m_get.assert_called_once_with(f"{BIG_CHAT_API}/events", params={"start_at": ANY})
         m_post.assert_called_once_with(
-            "http://localhost:8266/chats",
-            json={"external_id": str(conversation_id), "started_at": ANY}
+            f"{OUR_API}/chats", json={"external_id": str(conversation_id), "started_at": ANY}
         )
+
+    @patch("requests.get")
+    @patch("requests.post")
+    def test_pagination(self, m_post, m_get):
+        conversation_id = 12345
+        m_get.side_effect = [
+            MagicMock(
+                json=lambda: {
+                    "nextPageUrl": "whatever",
+                    "events": [{"event_name": "START", "conversation_id": conversation_id}],
+                },
+                status_code=200,
+            ),
+            MagicMock(
+                json=lambda: {
+                    "nextPageUrl": None,
+                    "events": [{"event_name": "START", "conversation_id": conversation_id + 1}],
+                },
+                status_code=200,
+            ),
+        ]
+
+        main.main()
+
+        assert m_get.call_args_list == [
+            call(f"{BIG_CHAT_API}/events", params={"start_at": ANY}),
+            call("whatever"),
+        ]
+        assert m_post.call_args_list == [
+            call(f"{OUR_API}/chats", json={"external_id": str(conversation_id), "started_at": ANY}),
+            call(f"{OUR_API}/chats", json={"external_id": str(conversation_id + 1), "started_at": ANY}),
+        ]
